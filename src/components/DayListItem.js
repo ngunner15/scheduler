77ef6d175304395f7ad22e987928236518d6ea49
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-18 15:43:46 -0500

    Update formatSpots function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,8 +9,8 @@ export default function DayListItem(props) {
       return "no spots remaining";
     } else if (spots === 1) {
       return "1 spot remaining";
-    } else if (spots === 2) {
-      return "2 spots remaining";
+    } else {
+      return `${spots} spots remaining`;
     }
   }
 
