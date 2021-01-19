04487e5c538e9677d3ce273acb1dc9cefc4ace6b
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-19 13:50:00 -0500

    Refractor

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,12 +9,12 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
+    <li className={interviewerClass} onClick = {props.setInterviewer}>
       <img className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.name}
+      {props.selected && props.name}
     </li>
   );
 }
