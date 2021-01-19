62a30b5510d033406d4d7b4b6b3ff3f0359e52f9
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-19 12:26:33 -0500

    Add css classes through className attribute

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "components/InterviewerListItem.scss"
+const classNames = require('classnames');
 
 export default function InterviewerListItem(props) {
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
-      <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)}>
+      <img className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
+      {props.name}
     </li>
   );
 }
