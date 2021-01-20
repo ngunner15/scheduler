aa85d8e5718bf1ef63b897cdd51411b4fced7a58
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-19 19:19:17 -0500

    Add stories of Form:Create Form:Edit

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -157,4 +158,22 @@ storiesOf("Appointment", module)
   />
   ))
   .add("Status", () => <Status message="Deleting" />)
-  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />);
+  .add("Error", () => (<Error
+    message="Could not delete appointment."
+    onClose={action("onClose")}
+  />
+  ))
+  .add("Create", () => (<Form
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />
+  ))
+  .add("Edit", () => (<Form
+    name="Nisarg"
+    interviewers={interviewers}
+    interviewer={interviewer.id}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />
+  ));
