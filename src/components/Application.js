299f95fc52efd736537240d889c425fd9564b006
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-20 16:18:41 -0500

    Add axios to fetch data for days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import "components/Appointment/styles.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  }
-];
 
 const appointments = [
   {
@@ -71,8 +54,16 @@ const appointments = [
 ];
 
 export default function Application(props) {
-
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    const testURL = `http://localhost:8001/api/days`;
+    axios.get(testURL).then(response => {
+      //console.log(response.data);
+      setDays(response.data);
+    });
+  }, [])
+  //console.log(days);
   const eachAppointment = appointments.map(item => <Appointment key={item.id} {...item} />)
 
   return (
