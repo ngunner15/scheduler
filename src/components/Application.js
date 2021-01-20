ecec485d2573c24534e375d5ff2323ee4ff619e7
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-20 18:42:53 -0500

    Add interviewer state in useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -12,7 +12,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay( state, state.day);
@@ -25,7 +26,7 @@ export default function Application(props) {
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2] }));
     })
   }, [])
 
