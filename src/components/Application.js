99f3713f24815ba223c77799893483483600887c
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-21 15:06:35 -0500

    Add getInterviewersForDay function to interviewers array and pass it to Appointment component

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import "components/Application.scss";
 import "components/Appointment/styles.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
-import { getAppointmentsForDay, getInterview } from "helpers/selectors"
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors"
 
 export default function Application(props) {
 
@@ -17,6 +17,7 @@ export default function Application(props) {
   });
 
   const dailyAppointments = getAppointmentsForDay( state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
   const setDay = day => setState({ ...state, day });
 
@@ -41,6 +42,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewers}
       />
     );
   });
