1f43644784eafae0ea932dd648afeb55c8946ff9
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-20 19:42:40 -0500

    add code to map over appointments using getInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -32,6 +32,19 @@ export default function Application(props) {
 
   const eachAppointment = dailyAppointments.map(item => <Appointment key={item.id} {...item} />)
 
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+  
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
