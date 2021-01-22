9092d510c51e41422f414052daef991639a3f991
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-22 12:21:00 -0500

    Create function save passed as prop for onSave

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,18 +2,32 @@ import React from "react";
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import Status from "./Status"
 import useVisualMode from "../../hooks/useVisualMode"
 import Form from "./Form";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+    .then(() => {
+      transition(SHOW);
+    });
+  }
 
   return (
     <article className="appointment">
@@ -25,7 +39,8 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()}/>}
+      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back()} onSave={save}/>}
+      {mode === SAVING && <Status message={SAVING}/>}
     </article>
   );
 }
