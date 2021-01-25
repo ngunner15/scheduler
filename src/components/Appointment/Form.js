ab4231429377eb8a68f9ab080d7162ed15915a58
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-25 18:53:44 -0500

    Add validate function to show error when name is empty

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setName("");
@@ -16,6 +17,17 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
+
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    
+    setError("");
+    props.onSave(name, interviewer);
+  };
+
 
   return(
     <main className="appointment__card appointment__card--create">
@@ -28,14 +40,16 @@ export default function Form(props) {
             value={name}
             onChange={(event) => setName(event.target.value)}
             placeholder="Enter Student Name"
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} setInterviewer={(id) => setInterviewer(id)} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={() => props.onCancel()}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
