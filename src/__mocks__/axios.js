1115c5655b2b43597f3d5b03914bd314fb66b5c8
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-26 14:59:28 -0500

    Add put and delete request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -52,3 +52,38 @@ const fixtures = {
     }
   }
 };
+
+export default {
+  defaults: { baseURL: "" },
+  get: jest.fn(url => {
+    if (url === "/api/days") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+
+    if (url === "/api/appointments") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
+    }
+
+    if (url === "/api/interviewers") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
+    }
+  }),
+  put: jest.fn(() => {
+    return Promise.resolve({ status: 204, statusText: "No Content" });
+  }),
+  delete: jest.fn(() => {
+    return Promise.resolve({ status: 204, statusText: "No Content" });
+  }),
+}
