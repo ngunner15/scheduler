a7f3f5b290a19fec3f43d42fd7bceaecb8e8f8a8
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-27 18:08:51 -0500

    Add validation for empty interviwer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,7 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
-    
+    if (interviewer === null) {
+      setError("Please select the interviewer");
+      return;
+    }
     setError("");
     props.onSave(name, interviewer);
   };
